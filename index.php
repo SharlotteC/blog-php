59f5e91d2e8ce77bc72d77cf4765c696d5b5c259
sharlotte <sharlotte.closson@gmail.com>
2022-05-24 13:54:09 +0200

    affichage par categories

--- index.php
+++ index.php
@@ -5,7 +5,27 @@
 
     if(file_exists($filename)) {
         $articles = json_decode(file_get_contents($filename), true) ?? [];
-        $categories = array_map(fn ($a) => $a["category"], $articles);
+        $catmap = array_map(fn ($a) => $a['category'], $articles);
+        
+        // je cree un tableau associatif qui a pour cles les vcategories et pour valeur le nombre d'articles
+        $categories = array_reduce($catmap, function($acc, $cat ){
+            if(isset($acc[$cat])){
+                $acc[$cat]++;
+            } else {
+                $acc[$cat] = 1;
+            }
+            return $acc;
+        }, []);
+
+        // je cree un tableau associatif qui a pour cles les categories et pour valeur tous les articles concernant la categorie
+        $articlesPerCategories = array_reduce($articles, function($acc, $article){
+            if(isset($acc[$article['category']])) {
+                $acc[$article['category']] = [...$acc[$article['category']], $article];
+            } else {
+                $acc[$article['category']] = [$article];
+            }
+            return $acc;
+        }, []);
     }
 ?>
 <!DOCTYPE html>
@@ -19,16 +39,23 @@
     <div class="container">
         <?php require_once 'includes/header.php'?>
         <div class="content">
-            <div class="articles-container">
-                <?php foreach($articles as $article) : ?>
-                    <div class="article block">
-                        <div class="overflow">
-                            <div class="img-container" style= "background-image : url(<?= $article['image'] ?>);"></div>
+            <div class="category-container">
+                <?php foreach($categories as $cat => $num) :?>
+                    <h2 class="p-10"><?= $cat ?></h2>
+                    <div class="articles-container">
+                    <?php foreach($articlesPerCategories[$cat] as $article) : ?>
+                        <div class="article block">
+                            <div class="overflow">
+                                <div class="img-container" style= "background-image : url(<?= $article['image'] ?>);"></div>
+                            </div>
+                            <h3><?= $article['title'] ?></h3>
                         </div>
-                        <h2><?= $article['title'] ?></h2>
-                    </div>
-                <?php endforeach; ?> 
+                    <?php endforeach; ?> 
+                </div>
+                <?php endforeach ?>
+
             </div>
+            
         </div>
         <?php require_once 'includes/footer.php'?>
     </div>
