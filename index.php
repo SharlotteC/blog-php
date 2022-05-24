f6d107d3e0c8314bad088df2aec72b441dc4a9ca
sharlotte <sharlotte.closson@gmail.com>
2022-05-24 13:12:29 +0200

    affichage des articles sur la page d'accueil

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+    $filename = __DIR__.'/data/articles.json';
+    $articles = [];
+    $categories = [];
 
+    if(file_exists($filename)) {
+        $articles = json_decode(file_get_contents($filename), true) ?? [];
+        $categories = array_map(fn ($a) => $a["category"], $articles);
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,11 +17,20 @@
 </head>
 <body>
     <div class="container">
-    <?php require_once 'includes/header.php'?>
-    <div class="content">
-
-    </div>
-    <?php require_once 'includes/footer.php'?>
+        <?php require_once 'includes/header.php'?>
+        <div class="content">
+            <div class="articles-container">
+                <?php foreach($articles as $article) : ?>
+                    <div class="article block">
+                        <div class="overflow">
+                            <div class="img-container" style= "background-image : url(<?= $article['image'] ?>);"></div>
+                        </div>
+                        <h2><?= $article['title'] ?></h2>
+                    </div>
+                <?php endforeach; ?> 
+            </div>
+        </div>
+        <?php require_once 'includes/footer.php'?>
     </div>
 </body>
 </html>
